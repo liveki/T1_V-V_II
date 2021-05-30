823e8e64c0168cfb12d7d73d08fd7d02aeb83d56
Francisco Lucca <francisco.lucca@pucrs.br>
2021-05-30 19:20:14 -0300

    Adicionando testes

--- src/test/java/com/t1/app/CondominioTest.java
+++ src/test/java/com/t1/app/CondominioTest.java
@@ -2,6 +2,7 @@ package com.t1.app;
 
 import java.util.Random;
 
+import com.t1.app.exceptions.MoradorInativoException;
 import com.t1.app.exceptions.OperadorComRegistrosException;
 
 import org.junit.jupiter.api.Assertions;
@@ -20,16 +21,16 @@ public class CondominioTest {
             c.excluirOperador(c.getOperadorAtual());
         });
     }
-    
+
     @Test
-    public void excluiOperadorSemEntregaVinculadaTest(){
+    public void excluiOperadorSemEntregaVinculadaTest() {
         Condominio c = new Condominio();
 
         Operador op = new Operador("Operador");
         c.incluirOperador(op);
 
         int expected = c.getOperadores().size() - 1;
-        
+
         try {
             c.excluirOperador(op);
         } catch (OperadorComRegistrosException e) {
@@ -40,13 +41,13 @@ public class CondominioTest {
     }
 
     @Test
-    public void excluiOperadorInexistenteTest(){
+    public void excluiOperadorInexistenteTest() {
         Condominio c = new Condominio();
 
         Operador op = new Operador("Operador");
-        
+
         int expected = c.getOperadores().size();
-        
+
         try {
             c.excluirOperador(op);
         } catch (OperadorComRegistrosException e) {
@@ -57,7 +58,7 @@ public class CondominioTest {
     }
 
     @Test
-    public void marcaMoradorInativoExistenteTest(){
+    public void marcaMoradorInativoExistenteTest() {
         Random r = new Random();
         Condominio c = new Condominio();
 
@@ -68,7 +69,7 @@ public class CondominioTest {
     }
 
     @Test
-    public void marcaMoradorInativoNaoExistenteTest(){
+    public void marcaMoradorInativoNaoExistenteTest() {
         Condominio c = new Condominio();
         Morador m = new Morador("nome", "rG", 1000, true);
 
@@ -78,50 +79,64 @@ public class CondominioTest {
     }
 
     @Test
-    public void marcaMoradorInativoJaInativoTest(){
+    public void marcaMoradorInativoJaInativoTest() {
         Random r = new Random();
         Condominio c = new Condominio();
 
-        Morador m = c.listarMoradores().stream().filter(mr-> mr.isAtivo() == false).findAny().orElse(c.listarMoradores().get(r.nextInt(c.listarMoradores().size())));
+        Morador m = c.listarMoradores().stream().filter(mr -> mr.isAtivo() == false).findAny()
+                .orElse(c.listarMoradores().get(r.nextInt(c.listarMoradores().size())));
 
         c.marcarMoradorInativo(m);
 
         Assertions.assertTrue(m.isAtivo() == false);
     }
-    
-    class marcarMoradorInativoTest {
+
+    @Test
+    public void retiraEntregaExistenteTest(){
+        Random r = new Random();
+        Condominio c = new Condominio();
+        c.definirOperadorAtual(c.getOperadores().get(r.nextInt(c.getOperadores().size())));
+        Entrega e = c.listarEntregasNaoRetiradas().get(r.nextInt(c.listarEntregasNaoRetiradas().size()));
+        Morador m = c.listarMoradores().stream().filter(
+            mr -> mr.getNroApartamento() == e.getAptoDestino()
+        ).findFirst().orElse(new Morador("Nome", "rg", e.getAptoDestino(), true));
+
+        try {
+            c.retirarEntrega(e, m);
+        } catch (MoradorInativoException e1) {
+            e1.printStackTrace();
+        }
+
+        Assertions.assertTrue(e.getRetiradaPor() == m);
 
     }
 
-    class encontrarMoradorTest {
+    @Test
+    public void retiraEntregaInexistenteTest(){
+        Random r = new Random();
+        Condominio c = new Condominio();
+        c.definirOperadorAtual(c.getOperadores().get(r.nextInt(c.getOperadores().size())));
+        Entrega e = new Entrega(c.getOperadorAtual(), "string", 101);
+        Morador m = c.listarMoradores().get(r.nextInt(c.listarMoradores().size()));
+
+        Assertions.assertThrows(IndexOutOfBoundsException.class, () -> {
+            c.retirarEntrega(e, m);
+        });
 
     }
 
-    class retirarEntregaTest {
+    @Test
+    public void retiraEntregaMoradorInativoTest(){
+        Random r = new Random();
+        Condominio c = new Condominio();
+        c.definirOperadorAtual(c.getOperadores().get(r.nextInt(c.getOperadores().size())));
+        Entrega e = c.listarEntregasNaoRetiradas().get(r.nextInt(c.listarEntregasNaoRetiradas().size()));
+        Morador m = c.listarMoradores().stream().filter(mr -> mr.isAtivo() == false).findAny()
+                .orElse(c.listarMoradores().get(r.nextInt(c.listarMoradores().size())));
+        m.setAtivo(false);
 
-    }
-
-    class encontrarEntregaTest {
-
-    }
-
-    class procurarEntregaPorDescricaoTest {
-
-    }
-
-    class carregarOperadoresTest {
-
-    }
-
-    class carregarMoradoresTest {
-
-    }
-
-    class carregarEntregasTest {
-
-    }
-
-    class procurarMoradorPorRGTest {
-
+        Assertions.assertThrows(MoradorInativoException.class, () -> {
+            c.retirarEntrega(e, m);
+        });
     }
 }
